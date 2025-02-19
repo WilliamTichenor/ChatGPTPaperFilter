16b1b364ae7cbaa5578a89d732b281dc142b14d7
William Tichenor <williamtichenor@tamu.edu>
2025-02-19 15:38:44 -0600

    start on task lock

--- paperfilter.py
+++ paperfilter.py
@@ -5,57 +5,77 @@ from openai import AsyncOpenAI, RateLimitError
 import rispy
 
 
-async def apiCall(entry):
+async def apiCall(index, entry):
   paperPrompt = f"""
     Title: {entry['title']}
     Year: {entry['year']}
     Abstract: {entry['notes_abstract']}
   """
+  if (index in backupDict):
+    return ["Backup", entry, backupDict[index], index]
   async with semaphore:
+    activeTasks += 1
     while True:
       try:
         response = await client.chat.completions.create(
           model="gpt-4o",
           messages=[sysPrompt, {"role": "user", "content": paperPrompt}]
         )
-        return [entry, response]
+        return ["Valid", entry, response, index]
       except RateLimitError as e:
         print("Rate limited! Waiting 10s...")
         await asyncio.sleep(10)
         continue
       except Exception as e:
 
-        return ["Error", e]
+        return ["Error", entry, e, index]
+
+async def backup():
+  while (True):
+    asyncio.sleep(10)
+    
 
 async def main():
   invalid = 0
   with open(fpath, 'r', encoding="utf-8-sig") as dataFile:
     data = rispy.load(dataFile)
-    tasks = [apiCall(entry) for entry in data]
+    tasks = [apiCall(index,entry) for index, entry in enumerate(data)]
+    backupTask = asyncio.create_task(backup())
 
-    i = 0
     for completed_task in asyncio.as_completed(tasks):
       response = await completed_task
-      i += 1
-      if (response[0]=="Error"):
-        print(str(i)+"/"+str(len(data))+": LLM Error: "+str(response[1]))
+      if (response[0]=="Backup"):
+        if (response[2]=="YES"):
+          yes.append(response[1])
+          print(str(response[3])+"/"+str(len(data))+": BACKUP, YES: "+response[1]['title'])
+        elif (response[2]=="NO"):
+          no.append(response[1])
+          print(str(response[3])+"/"+str(len(data))+": BACKUP, NO: "+response[1]['title'])
+        elif (response[2]=="MAYBE"):
+          no.append(response[1])
+          print(str(response[3])+"/"+str(len(data))+": BACKUP, MAYBE: "+response[1]['title'])
+        else:
+          print(str(response[3])+"/"+str(len(data))+": Backup Corrupted:"+response[1]['title']+": Please delete and rerun")
+      elif (response[0]=="Error"):
+        print(str(response[3])+"/"+str(len(data))+": LLM Error: "+str(response[1]))
         invalid += 1
-      elif (response[1].choices[0].message.content == "YES"):
-        yes.append(response[0])
-        print(str(i)+"/"+str(len(data))+": YES: "+response[0]['title'])
-      elif (response[1].choices[0].message.content == "NO"):
-        no.append(response[0])
-        print(str(i)+"/"+str(len(data))+": NO: "+response[0]['title'])
-      elif (response[1].choices[0].message.content == "MAYBE"):
-        maybe.append(response[0])
-        print(str(i)+"/"+str(len(data))+": MAYBE: "+response[0]['title'])
+      elif (response[2].choices[0].message.content == "YES"):
+        yes.append(response[1])
+        backupDict[response[3]] = "YES"
+        print(str(response[3])+"/"+str(len(data))+": YES: "+response[1]['title'])
+      elif (response[2].choices[0].message.content == "NO"):
+        no.append(response[1])
+        backupDict[response[3]] = "NO"
+        print(str(response[3])+"/"+str(len(data))+": NO: "+response[1]['title'])
+      elif (response[2].choices[0].message.content == "MAYBE"):
+        maybe.append(response[1])
+        backupDict[response[3]] = "MAYBE"
+        print(str(response[3])+"/"+str(len(data))+": MAYBE: "+response[1]['title'])
       else:
-        print(str(i)+"/"+str(len(data))+": Invalid answer: "+response[1].choices[0].message.content)
+        print(str(response[3])+"/"+str(len(data))+": Invalid answer: "+response[1]['title']+": "+response[1].choices[0].message.content)
         invalid += 1
-        continue
-        
-         
-        
+      activeTasks -= 1
+                        
     print("Filter Complete!")
     print(str(len(yes))+" "+"yes")
     print(str(len(no))+" "+"no")
@@ -90,11 +110,12 @@ sysPrompt = {"role": "system", "content": """
 yes = []
 no = []
 maybe = []
-contextLimit = 10
-context = []
+backupDict = {}
 
 SEMAPHORE_LIMIT = 8
 semaphore = asyncio.Semaphore(SEMAPHORE_LIMIT)
+backupLock = asyncio.Lock()
+activeTasks = 0
 
 fpath = "testing500.ris"
 
