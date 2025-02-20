1cffb0e243c6d71be9b4dfece6300dd750cbc083
William Tichenor <williamtichenor@tamu.edu>
2025-02-19 23:21:49 -0600

    backup feature added

--- paperfilter.py
+++ paperfilter.py
@@ -1,9 +1,14 @@
 import os
+import pickle
 import asyncio
 from dotenv import load_dotenv
 from openai import AsyncOpenAI, RateLimitError
 import rispy
 
+
+SEMAPHORE_LIMIT = 7 #If you're getting rate limited too much, drop this!
+GPTMODEL = "gpt-4o" #Tested with gpt-4o and gpt-4o-mini
+BACKUPDELAY = 900 #Time, in seconds, between backups
 
 async def apiCall(index, entry):
   paperPrompt = f"""
@@ -14,11 +19,15 @@ async def apiCall(index, entry):
   if (index in backupDict):
     return ["Backup", entry, backupDict[index], index]
   async with semaphore:
-    activeTasks += 1
+    async with backupLock:
+      pass
+    async with activeTaskLock:
+      global activeTasks
+      activeTasks += 1
     while True:
       try:
         response = await client.chat.completions.create(
-          model="gpt-4o",
+          model=GPTMODEL,
           messages=[sysPrompt, {"role": "user", "content": paperPrompt}]
         )
         return ["Valid", entry, response, index]
@@ -27,13 +36,19 @@ async def apiCall(index, entry):
         await asyncio.sleep(10)
         continue
       except Exception as e:
-
         return ["Error", entry, e, index]
 
 async def backup():
   while (True):
-    asyncio.sleep(10)
-    
+    await asyncio.sleep(BACKUPDELAY)
+    async with backupLock:
+      print("Preparing for backup...")
+      while (activeTasks > 0):
+        await asyncio.sleep(1)
+      print("Backup activated!")
+      with open(fpath+".sav","wb") as backupFile:
+        backupFile.truncate(0)
+        pickle.dump(backupDict, backupFile)
 
 async def main():
   invalid = 0
@@ -42,40 +57,46 @@ async def main():
     tasks = [apiCall(index,entry) for index, entry in enumerate(data)]
     backupTask = asyncio.create_task(backup())
 
+    i = 1
     for completed_task in asyncio.as_completed(tasks):
       response = await completed_task
+      print(str(i)+"/"+str(len(data))+":")
+      i += 1
       if (response[0]=="Backup"):
         if (response[2]=="YES"):
           yes.append(response[1])
-          print(str(response[3])+"/"+str(len(data))+": BACKUP, YES: "+response[1]['title'])
+          print(str(response[3])+": BACKUP, YES: "+response[1]['title'])
         elif (response[2]=="NO"):
           no.append(response[1])
-          print(str(response[3])+"/"+str(len(data))+": BACKUP, NO: "+response[1]['title'])
+          print(str(response[3])+": BACKUP, NO: "+response[1]['title'])
         elif (response[2]=="MAYBE"):
-          no.append(response[1])
-          print(str(response[3])+"/"+str(len(data))+": BACKUP, MAYBE: "+response[1]['title'])
+          maybe.append(response[1])
+          print(str(response[3])+": BACKUP, MAYBE: "+response[1]['title'])
         else:
-          print(str(response[3])+"/"+str(len(data))+": Backup Corrupted:"+response[1]['title']+": Please delete and rerun")
+          print(str(response[3])+": Backup Corrupted:"+response[1]['title']+": Please delete and rerun")
       elif (response[0]=="Error"):
-        print(str(response[3])+"/"+str(len(data))+": LLM Error: "+str(response[1]))
+        print(str(response[3])+": LLM Error: "+str(response[1]))
         invalid += 1
       elif (response[2].choices[0].message.content == "YES"):
         yes.append(response[1])
         backupDict[response[3]] = "YES"
-        print(str(response[3])+"/"+str(len(data))+": YES: "+response[1]['title'])
+        print(str(response[3])+": YES: "+response[1]['title'])
       elif (response[2].choices[0].message.content == "NO"):
         no.append(response[1])
         backupDict[response[3]] = "NO"
-        print(str(response[3])+"/"+str(len(data))+": NO: "+response[1]['title'])
+        print(str(response[3])+": NO: "+response[1]['title'])
       elif (response[2].choices[0].message.content == "MAYBE"):
         maybe.append(response[1])
         backupDict[response[3]] = "MAYBE"
-        print(str(response[3])+"/"+str(len(data))+": MAYBE: "+response[1]['title'])
+        print(str(response[3])+": MAYBE: "+response[1]['title'])
       else:
-        print(str(response[3])+"/"+str(len(data))+": Invalid answer: "+response[1]['title']+": "+response[1].choices[0].message.content)
+        print(str(response[3])+": Invalid answer: "+response[1]['title']+": "+response[2].choices[0].message.content)
         invalid += 1
-      activeTasks -= 1
+      async with activeTaskLock:
+        global activeTasks
+        activeTasks -= 1
                         
+    backupTask.cancel()
     print("Filter Complete!")
     print(str(len(yes))+" "+"yes")
     print(str(len(no))+" "+"no")
@@ -88,6 +109,8 @@ async def main():
             rispy.dump(no, dataFile)
     with open("output/maybe.ris", 'w', encoding="utf-8-sig") as dataFile:
             rispy.dump(maybe, dataFile)
+    if os.path.isfile(fpath+".sav"):
+      os.remove(fpath+".sav")
 
 
 load_dotenv()
@@ -107,16 +130,22 @@ sysPrompt = {"role": "system", "content": """
   Give your answer as "NO" if any criteria are clearly violated
 """}
 
+fpath = "testing500.ris"
+
 yes = []
 no = []
 maybe = []
 backupDict = {}
+try:
+  with open(fpath+".sav", "rb") as backupFile:
+    backupDict = pickle.load(backupFile)
+    print("Backup loaded with "+str(len(backupDict))+" records")
+except Exception as e:
+  pass
 
-SEMAPHORE_LIMIT = 8
 semaphore = asyncio.Semaphore(SEMAPHORE_LIMIT)
 backupLock = asyncio.Lock()
+activeTaskLock = asyncio.Lock()
 activeTasks = 0
-
-fpath = "testing500.ris"
 
 asyncio.run(main())
